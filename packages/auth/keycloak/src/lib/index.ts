8ae973a799f8cc3a9e8b4c568fef1aa0a679e964
Simon Novak <25969+snovak7@users.noreply.github.com>
2025-11-18 18:18:59 +0100

    feat: Export OIDC types and middleware from Keycloak package

--- packages/auth/keycloak/src/lib/index.ts
+++ packages/auth/keycloak/src/lib/index.ts
@@ -1,1 +1,8 @@
 // Reexport your entry components here
+import {type OidcConfig} from "./types.ts";
+import {OidcMiddleware} from "./middleware.ts";
+
+export {
+    type OidcConfig,
+    OidcMiddleware
+};
